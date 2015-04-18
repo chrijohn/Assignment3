ec2f9319122633e3f901c1726deb1d9565b2ef93
chrijohn <chrijohn@onid.oregonstate.edu>
2015-04-18 16:11:26 -0500

    completed all but last question in function.js

--- assignment3/src/functions.js
+++ assignment3/src/functions.js
@@ -13,7 +13,10 @@
 */
 
 //your code here
-
+function uselessFunction()
+{
+	return null;
+}
 //end your code
 
 var bar = 'not a function';
@@ -30,7 +33,19 @@ var barType = typeof bar;
 */
 
 //your code here
-
+var bar = function(doubleArray)
+{
+	for (var i=0; i <doubleArray.length;i++)
+	{
+		doubleArray[i]= (doubleArray[i] * 2);
+		if (isNaN(doubleArray[i]))
+		{
+			return false;
+		}
+	}
+	
+	return true;
+}
 //end your code
 
 /**
